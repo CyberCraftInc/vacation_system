ad252371965bfe2a123818c189bee7dcd4a4f5c6
Oleg «Eleidan» Kulyk <helh.saintman@gmail.com>
2015-07-01 12:54:39 +0300

    Implement vacations filter based on date range.

--- app/assets/javascripts/views/vacation_requests_list.js
+++ app/assets/javascripts/views/vacation_requests_list.js
@@ -4,8 +4,8 @@ App.Views.VacationRequestsList = Backbone.View.extend({
 
   events: {
     'change select[name=vacation-status]':  'onStatusChange',
-    // 'change input[name=from]':           'onFromDate',
-    // 'change input[name=to]':             'onToDate',
+    'change input[name=from]':              'onDateFrom',
+    'change input[name=to]':                'onDateTo',
   },
 
   initialize: function() {
@@ -14,19 +14,23 @@ App.Views.VacationRequestsList = Backbone.View.extend({
     // Prepare handy access for the controls
     this.$table   = this.$('table.vacation-requests');
     this.$select  = this.$('select[name=vacation-status]');
+    this.$from    = this.$('input[name=from]');
+    this.$to      = this.$('input[name=to]');
 
     this.listenTo(this.collection,  'sync',  this.render);
     this.listenTo(this.collection,  'all',  this.logger);
 
     this.collection.fetch();
     this.statusFilter = null;
+    this.dateFrom = null;
+    this.dateTo   = null;
   },
 
   render: function() {
     this.$table.empty();
+
     this.collection.each(function(model) {
-      var isVisible = _.include(this.statusFilter, model.get('status'));
-      if (isVisible) {
+      if (this.isVacationVisible(model)) {
         var item = new App.Views.VacationRequest({'model': model});
         this.$table.append(item.render().$el);
       }
@@ -38,7 +42,41 @@ App.Views.VacationRequestsList = Backbone.View.extend({
   onStatusChange: function( e ) {
     this.statusFilter = this.$select.val();
     this.render();
-    // console.log(this.statusFilter);
+  },
+
+  onDateFrom: function( e ) {
+    this.dateFrom = this.$from.val();
+    this.render();
+  },
+
+  onDateTo: function( e ) {
+    this.dateTo = this.$to.val();
+    console.log(this.dateTo);
+    this.render();
+  },
+
+  // Convert date's string into it's miliseconds representation
+  dateToMS: function( date ) {
+    var dateInMS = (new Date(date)).getTime();
+
+    var result = date ? dateInMS : 0;
+    return result;
+  },
+
+  // Check if given model should be visible
+  isVacationVisible: function( model ) {
+    var isVisible = false;
+    // Calculate date in miliseconds to compare later
+    var dateFrom  = this.dateToMS(this.dateFrom);
+    var dateTo    = this.dateToMS(this.dateTo);
+    var modelFrom = this.dateToMS(model.get('start'));
+
+    var hasProperStatus = _.include(this.statusFilter, model.get('status'));
+    var hasProperDateRange = (modelFrom >= dateFrom) && ((modelFrom <= dateTo) || (dateTo === 0));
+
+    isVisible = hasProperStatus && hasProperDateRange;
+
+    return isVisible;
   },
 
   logger: function( e, p ) {
