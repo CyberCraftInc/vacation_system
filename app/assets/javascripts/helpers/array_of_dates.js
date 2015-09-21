00da2ad199fd706b4d8f7ded68777d6c0e314fb3
Oleg «Eleidan» Kulyk <helh.saintman@gmail.com>
2015-09-21 11:53:49 +0300

    Update according to comments
    Refactor arrayOfDates() helper so that it uses functional approach.

--- app/assets/javascripts/helpers/array_of_dates.js
+++ app/assets/javascripts/helpers/array_of_dates.js
@@ -1,20 +1,18 @@
 // Returns array of date strings formatted as 'YYYY-MM-DD'.
 App.Helpers.arrayOfDates = function(start, duration) {
   var count;
-  var date = '',
-      dateRegExMatcher = /^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/,
+  var dateRegExMatcher = /^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/,
       isValidDate = false,
       result = [];
 
-  isValidDate = (typeof start === 'string') && dateRegExMatcher.test(start);
+  isValidDate = _.isString(start) && dateRegExMatcher.test(start);
   if (!isValidDate) {
     return result;
   }
 
-  for (count = 0; count < duration; count++) {
-    date = moment(start, 'YYYY-MM-DD').add(count,'days').format('YYYY-MM-DD');
-    result.push(date);
-  }
+  result = _.chain(_.range(duration)).map(function(count) {
+    return moment(start, 'YYYY-MM-DD').add(count,'days').format('YYYY-MM-DD');
+  }).value();
 
   return result;
 };
