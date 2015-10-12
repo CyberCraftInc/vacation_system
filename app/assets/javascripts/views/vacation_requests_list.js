87fd4727ce6e24ca7727806703f870b0a481592f
Oleg «Eleidan» Kulyk <helh.saintman@gmail.com>
2015-10-12 18:38:27 +0300

    Implement enhancement described in issue #27
    Update App.Views.VacationRequestsList so that it is possible to cancel
    any vacation with status other than 'cancelled'.
    Also, search filter is enabled. This functionality is provided
    by the plugin out of box. For details on search filter see
    http://bootstrap-table.wenzhixin.net.cn/documentation/#table-options
    
    The search filter is supposed to replace previous filter mechanism
    implementation.

--- app/assets/javascripts/views/vacation_requests_list.js
+++ app/assets/javascripts/views/vacation_requests_list.js
@@ -2,10 +2,10 @@ App.Views.VacationRequestsList = Backbone.View.extend({
   el: '.vacation-requests-list',
   template: JST['templates/vacation_requests_list'],
 
-  events: {
-    'change select[name=vacation-status]':  'onStatusChange',
-    'change input[name=from]':              'onDateFrom',
-    'change input[name=to]':                'onDateTo',
+  operationsEvents: function() {
+    return {
+      'click button[name=cancel]': this.onCancel
+    };
   },
 
   initialize: function(options) {
@@ -13,19 +13,13 @@ App.Views.VacationRequestsList = Backbone.View.extend({
 
     this.listenTo(this.collection, 'sync',  this.renderTable);
 
-    this.statusFilter = null;
-    this.dateFrom = null;
-    this.dateTo   = null;
+    this.onCancel = _.bind(this.onCancel, this);
   },
 
   render: function() {
     this.$el.html(this.template());
 
-    this.$table     = this.$('table.vacation-requests');
-    this.$tableRows = this.$('table.vacation-requests tbody');
-    this.$select    = this.$('select[name=vacation-status]');
-    this.$from      = this.$('input[name=from]');
-    this.$to        = this.$('input[name=to]');
+    this.$table = this.$('table.vacation-requests');
 
     this.renderTable();
 
@@ -33,62 +27,64 @@ App.Views.VacationRequestsList = Backbone.View.extend({
   },
 
   renderTable: function() {
-    var numberOfVisibleVacations = 0;
-    this.$tableRows.empty();
-    // Hide empty table
-    this.$table.hide();
+    this.$table.bootstrapTable('destroy');
+    this.$table.bootstrapTable({
+      search: true,
+      data: this.collection.toJSON(),
+      columns: [{
+          field: 'start_date',
+          title: 'Start date',
+          valign: 'middle',
+          sortable: true
+      }, {
+          field: 'actual_end_date',
+          title: 'End date',
+          valign: 'middle',
+          sortable: true
+      }, {
+          field: 'kind',
+          title: 'Type',
+          align: 'center',
+          valign: 'middle',
+          sortable: true
+      }, {
+          field: 'status',
+          title: 'Status',
+          align: 'center',
+          valign: 'middle',
+          sortable: true
+      }, {
+          field: 'operations',
+          title: 'Operations',
+          align: 'center',
+          events: this.operationsEvents(),
+          formatter: this.ownerOperationsFormatter,
+          width: '15%',
+          sortable: false
+      }],
+    });
+  },
 
-    this.collection.each(function(model) {
-      if (this.isVacationVisible(model)) {
-        numberOfVisibleVacations++;
-        var item = new App.Views.VacationRequest({'model': model});
-        this.$tableRows.append(item.render().$el);
-      }
-    }, this);
+  onCancel: function(event, value, row, index) {
+    var that = this;
 
-    if (numberOfVisibleVacations > 0) {
-      this.$table.show();
+    $.get('vacation_requests/'+row.id.toString()+'/cancel')
+      .done(function() {
+        // TODO: implement notification, if needed
+        // Trigger table update
+        that.collection.fetch();
+      })
+      .fail(function(response) {
+        // TODO: implement notification
+        console.error('FAIL');
+      });
+  },
+
+  ownerOperationsFormatter: function(value, row, index) {
+    if (row.status === 'cancelled') {
+      return '';
     }
-  },
 
-  onStatusChange: function(event) {
-    this.statusFilter = this.$select.val();
-    this.renderTable();
-  },
-
-  onDateFrom: function(event) {
-    this.dateFrom = this.$from.val();
-    this.render();
-  },
-
-  onDateTo: function(event) {
-    this.dateTo = this.$to.val();
-    this.render();
-  },
-
-  // Convert date's string into it's miliseconds representation
-  // TODO: refactor the following code to use just Date objects
-  // as they are comparable by default.
-  dateToMS: function(date) {
-    var dateInMS = (new Date(date)).getTime();
-
-    var result = date ? dateInMS : 0;
-    return result;
-  },
-
-  // Check if given model should be visible
-  isVacationVisible: function(model) {
-    var isVisible = false;
-    // Calculate date in miliseconds to compare later
-    var dateFrom  = this.dateToMS(this.dateFrom);
-    var dateTo    = this.dateToMS(this.dateTo);
-    var modelFrom = this.dateToMS(model.get('start_date'));
-
-    var hasProperStatus = _.include(this.statusFilter, model.get('status'));
-    var hasProperDateRange = (modelFrom >= dateFrom) && ((modelFrom <= dateTo) || (dateTo === 0));
-
-    isVisible = hasProperStatus && hasProperDateRange;
-
-    return isVisible;
-  },
+    return JST['templates/approval_request_owner_operations']();
+  }
 });
