a6baaae1d725e5f2d773be92e50531198a1f7ef8
Oleg «Eleidan» Kulyk <helh.saintman@gmail.com>
2015-11-23 16:36:34 +0200

    Update number of days for sickness leave

--- lib/available_vacations/calculus.rb
+++ lib/available_vacations/calculus.rb
@@ -2,7 +2,7 @@ module AvailableVacations
   DAYS_IN_YEAR = 365
   # Number of available days per year for each type of vacation
   PLANNED_PER_YEAR = 20
-  SICKNESS_PER_YEAR = 21
+  SICKNESS_PER_YEAR = 15
 
   # Number of available days per day for each type of vacation
   PLANNED_PER_DAY = PLANNED_PER_YEAR.fdiv(DAYS_IN_YEAR)
